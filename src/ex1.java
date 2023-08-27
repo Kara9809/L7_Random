287b73bc313923a3167f8a03fc92fbe559cb6f31
Karina <vip.kara0998@mail.ru>
2023-08-27 23:39:16 +0300

    12

--- src/ex1.java
+++ src/ex1.java
@@ -1,2 +1,12 @@
-package PACKAGE_NAME;public class ex1 {
-}
+public class ex1 {
+    public static void main(String[] args) {
+            int a = (int) (Math.random()*10)+1;
+            int b = (int) (Math.random()*10)+1;
+            int c = (int) (Math.random()*10)+1;
+            System.out.println("a : "+a);
+            System.out.println("b : "+b);
+            System.out.println("c : "+c);
+            System.out.println("Sum : " + (a + b +c));
+        }
+    }
+
--- src/ex2.java
+++ src/ex2.java
@@ -1,2 +1,11 @@
-package PACKAGE_NAME;public class ex2 {
+public class ex2 {
+    public static void main(String[] args) {
+        int a = (int) (Math.random()*10)+1;
+        int b = (int) (Math.random()*10)+1;
+        int c = (int) (Math.random()*10)+1;
+        System.out.println("a : "+a);
+        System.out.println("b : "+b);
+        System.out.println("c : "+c);
+        System.out.println("Avg : "+(a + b +c)/3);
+    }
 }
